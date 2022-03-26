956f01645ecb0505c3c8eccf58b4d64bdbb26aa9
ins4w <amejia.foo@gmail.com>
2022-03-26 02:49:48 -0500

    feat: support for scrapping followers usernames

--- public/scripts/scrapper.js
+++ public/scripts/scrapper.js
@@ -5,19 +5,16 @@
     return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
   };
 
-  // dist/modules/Person.js
-  var require_Person = __commonJS({
-    "dist/modules/Person.js"(exports) {
+  // dist/utils/evaluators.js
+  var require_evaluators = __commonJS({
+    "dist/utils/evaluators.js"(exports) {
       "use strict";
       Object.defineProperty(exports, "__esModule", { value: true });
-      exports.Person = void 0;
-      var Person = class {
-        nombre;
-        constructor(nombre) {
-          this.nombre = nombre;
-        }
+      exports.xpathEval = void 0;
+      var xpathEval = (expression, node) => {
+        return document.evaluate(expression, node, null, XPathResult.ANY_TYPE, null);
       };
-      exports.Person = Person;
+      exports.xpathEval = xpathEval;
     }
   });
 
@@ -25,10 +22,16 @@
   var require_scrapper = __commonJS({
     "dist/scripts/scrapper.js"(exports) {
       Object.defineProperty(exports, "__esModule", { value: true });
-      var Person_1 = require_Person();
-      var me = new Person_1.Person("Angelo");
+      var evaluators_1 = require_evaluators();
+      var followers = [];
+      var followersSpanTags = (0, evaluators_1.xpathEval)("//span[@class='Link--secondary pl-1' or @class='Link--secondary']", document);
+      var followersIterator = followersSpanTags.iterateNext();
+      while (followersIterator) {
+        followers.push(followersIterator.textContent);
+        followersIterator = followersSpanTags.iterateNext();
+      }
       var port = chrome.runtime.connect({ name: "safePort" });
-      port.postMessage({ me });
+      port.postMessage(followers);
     }
   });
   "use strict";
--- src/scripts/scrapper.ts
+++ src/scripts/scrapper.ts
@@ -1,5 +1,14 @@
 import { Person } from "../modules/Person";
+import { xpathEval } from "../utils/evaluators"
 
-let me = new Person("Angelo")
+let followers = []
+let followersSpanTags = xpathEval("//span[@class='Link--secondary pl-1' or @class='Link--secondary']", document);
+let followersIterator = followersSpanTags.iterateNext();
+
+while (followersIterator) {
+    followers.push(followersIterator.textContent);
+    followersIterator = followersSpanTags.iterateNext();
+}
+
 let port = chrome.runtime.connect({ name: "safePort" });
-port.postMessage({me})
+port.postMessage(followers)
