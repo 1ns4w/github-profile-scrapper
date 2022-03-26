6a50f654470fa347577a53238362e1345d06dfca
Insaw <amejia.foo@gmail.com>
2022-03-26 00:48:56 -0500

    test: remove dist from remote

--- dist/scripts/scrapper.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Person_1 = require("../modules/Person");
-let me = new Person_1.Person("Angelo");
-let port = chrome.runtime.connect({ name: "safePort" });
-port.postMessage({ me });
