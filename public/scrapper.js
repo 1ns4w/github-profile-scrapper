6677df5492cd5e720396e81b153187510c22acca
ins4w <amejia.foo@gmail.com>
2022-03-26 20:30:29 -0500

    fix: name with more than 1 word issue

--- public/scrapper.js
+++ public/scrapper.js
@@ -46,10 +46,10 @@
       var evaluators_1 = require_evaluators();
       var nameTag = (0, evaluators_1.xpathEval)("//span[@itemprop = 'name']", document);
       var nameTagIterator = nameTag.iterateNext();
-      var nameTagValue = nameTagIterator?.textContent?.split(/[^\w]+/)[1];
+      var nameTagValue = nameTagIterator?.textContent?.match(/[(a-z)(0-9)]+/ig)?.join(" ");
       var usernameTag = (0, evaluators_1.xpathEval)("//span[@itemprop = 'additionalName']", document);
       var usernameTagIterator = usernameTag.iterateNext();
-      var usernameTagValue = usernameTagIterator?.textContent?.match(/[(a-z)(0-9)]+/ig)?.join(" ");
+      var usernameTagValue = usernameTagIterator?.textContent?.split(/[(a-z)(0-9)]+/i)[0];
       var avatarURLTag = (0, evaluators_1.xpathEval)("//div[@class ='js-profile-editable-replace']//img[contains(@class, 'avatar-user')]", document);
       var avatarURLTagIterator = avatarURLTag.iterateNext();
       var avatarURLTagValue = avatarURLTagIterator ? avatarURLTagIterator.src : "";
--- src/scrapper.ts
+++ src/scrapper.ts
@@ -3,11 +3,11 @@ import { xpathEval } from "./modules/utils/evaluators"
 
 let nameTag = xpathEval("//span[@itemprop = 'name']", document);
 let nameTagIterator = nameTag.iterateNext();
-let nameTagValue = nameTagIterator?.textContent?.split(/[^\w]+/)[1]
+let nameTagValue = nameTagIterator?.textContent?.match(/[(a-z)(0-9)]+/ig)?.join(' ')
 
 let usernameTag = xpathEval("//span[@itemprop = 'additionalName']", document);
 let usernameTagIterator = usernameTag.iterateNext();
-let usernameTagValue = usernameTagIterator?.textContent?.match(/[(a-z)(0-9)]+/ig)?.join(' ')
+let usernameTagValue = usernameTagIterator?.textContent?.split(/[(a-z)(0-9)]+/i)[0]
 
 let avatarURLTag = xpathEval("//div[@class ='js-profile-editable-replace']//img[contains(@class, 'avatar-user')]", document);
 let avatarURLTagIterator = <HTMLImageElement> avatarURLTag.iterateNext();
