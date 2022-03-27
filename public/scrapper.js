c15da768a76fb0597a4c8e9d17b03bbe36e2531f
ins4w <amejia.foo@gmail.com>
2022-03-26 22:35:27 -0500

    chore: refactor regex

--- public/scrapper.js
+++ public/scrapper.js
@@ -46,13 +46,13 @@
       var evaluators_1 = require_evaluators();
       var nameTag = (0, evaluators_1.xpathEval)("//span[@itemprop = 'name']", document);
       var nameTagIterator = nameTag.iterateNext();
-      var nameTagValue = nameTagIterator?.textContent?.match(/[(a-z)(0-9)]+/ig)?.join(" ");
+      var nameTagValue = nameTagIterator?.textContent?.match(/[^\s|\n]+/ig)?.join(" ");
       var usernameTag = (0, evaluators_1.xpathEval)("//span[@itemprop = 'additionalName']", document);
       var usernameTagIterator = usernameTag.iterateNext();
-      var usernameTagValue = usernameTagIterator?.textContent?.match(/[(a-z)(0-9)]+/i)?.join();
+      var usernameTagValue = usernameTagIterator?.textContent?.match(/[a-z|0-9|\-]+/i)?.join();
       var avatarURLTag = (0, evaluators_1.xpathEval)("//div[@class ='js-profile-editable-replace']//img[contains(@class, 'avatar-user')]", document);
       var avatarURLTagIterator = avatarURLTag.iterateNext();
-      var avatarURLTagValue = avatarURLTagIterator ? avatarURLTagIterator.src : "";
+      var avatarURLTagValue = avatarURLTagIterator.src;
       var port = chrome.runtime.connect({ name: "safePort" });
       port.postMessage(new User_1.User(nameTagValue, usernameTagValue, avatarURLTagValue));
     }
--- src/scrapper.ts
+++ src/scrapper.ts
@@ -3,11 +3,11 @@ import { xpathEval } from "./modules/utils/evaluators"
 
 let nameTag = xpathEval("//span[@itemprop = 'name']", document);
 let nameTagIterator = nameTag.iterateNext();
-let nameTagValue = nameTagIterator?.textContent?.match(/[(a-z)(0-9)]+/ig)?.join(' ')
+let nameTagValue = nameTagIterator?.textContent?.match(/[^\s|\n]+/ig)?.join(' ')
 
 let usernameTag = xpathEval("//span[@itemprop = 'additionalName']", document);
 let usernameTagIterator = usernameTag.iterateNext();
-let usernameTagValue = usernameTagIterator?.textContent?.match(/[(a-z)(0-9)]+/i)?.join()
+let usernameTagValue = usernameTagIterator?.textContent?.match(/[a-z|0-9|\-]+/i)?.join()
 
 let avatarURLTag = xpathEval("//div[@class ='js-profile-editable-replace']//img[contains(@class, 'avatar-user')]", document);
 let avatarURLTagIterator = <HTMLImageElement> avatarURLTag.iterateNext();
